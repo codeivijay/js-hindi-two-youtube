7f31b68fe14ecc11baa360678e68b67c7b552e67
Vijay Singh Diggi <105920990+webdevvjay@users.noreply.github.com>
2024-03-29 12:23:29 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+// let score = '33'
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+let score = '33abc'
+let scoreNull = 'vijay'
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(scoreNull)
+//console.log(valueInNumber)
+
+// "33" => 33
+//"33abc"=> NaN
+//true => 1; false => 0
+
+let isLoggedIn = 'vijay'
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
