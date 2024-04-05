206e58b4acd4e7f3365a46c82f8af48e34c905cd
Vijay Singh Diggi <105920990+webdevvjay@users.noreply.github.com>
2024-04-05 14:06:35 +0000

    function startting

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,15 @@
+function leeta() {
+    console.log("v")
+    console.log("i")
+    console.log("j")
+    console.log("a")
+    console.log("y")
+}
+
+//leeta()
+
+function addTwoNumbers(num1, num2) {
+    console.log(num1 + num2)
+}
+
+addTwoNumbers(2 , 4)
