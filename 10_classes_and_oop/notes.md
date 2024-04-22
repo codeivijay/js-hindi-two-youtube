5e5b6827d343a81f6911c1b98e05c11b0d92d565
Vijay Singh Diggi <105920990+webdevvjay@users.noreply.github.com>
2024-04-22 16:31:20 +0530

    notes and oop

---
+++ 10_classes_and_oop/notes.md
@@ -0,0 +1,24 @@
+# javascript and classes
+
+## OOP - its a paradiagm
+
+## Object
+- collection of properties and methods
+- toLowerCase
+
+## why use OOP 
+
+## parts of OOP
+
+- Object literal
+
+- Constructor function
+- Prototypes
+- Classes
+- Instances (new, this)
+
+## 4 Pillars 
+- Abstraction
+- Encapsulation
+- Inheritance
+- Polymorphism 
---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,57 @@
+const user = {
+    username: "vijay",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function() {
+        //console.log("Got user details from database")
+        //console.log(`Username: ${this.username}`);
+        console.log(this)
+    }
+
+
+}
+
+const user2 = {
+    username: "vijay diggi",
+    loginCount: 10,
+    signedIn: true,
+
+    getUserDetails: function() {
+        //console.log("Got user details from database")
+        //console.log(`Username: ${this.username}`);
+        console.log(this)
+    }
+
+
+}
+
+//console.log(user.username)
+//console.log(user.getUserDetails())
+//console.log(this)
+
+//const promiseOne = new Promise()
+
+function User(username, loginCount, isLoggedIn) {
+    this.username = username
+    this.loginCount = loginCount
+    this.isLoggedIn = isLoggedIn
+
+    this.gretting = function() {
+        console.log(`welcome ${this.username}`)
+    }
+    
+    return this
+}
+
+const userOne = new User("Vijay", 12, true)
+const userTwo = new User("Chai aur Code", 18, false)
+
+console.log(userOne.constructor)
+//console.log(userTwo)
+
+// new - keyword is constructor function
+
+/* 
+
+ */
