7afe5bae5835b10d029f054c2eb5b5c5074400dd
Vijay Singh Diggi <105920990+webdevvjay@users.noreply.github.com>
2024-04-23 14:05:21 +0530

    setprototype, Object.prototye.name = function() {...}, __proto__

--- 10_classes_and_oop/prototype.js
+++ 10_classes_and_oop/prototype.js
@@ -14,4 +14,53 @@ let heroPower = {
     }
 }
 
-heroPower.vijay()
+Object.prototype.vijay = function() {
+    console.log(`vijay is present in all objects`)
+}
+
+Array.prototype.vivek = function() {
+    console.log(`Hey i am vivek`)
+}
+// heroPower.vijay()
+// myHeros.vijay()
+// myHeros.vivek()
+// heroPower.vivek()
+
+// Inheritance 
+
+const User = {
+    name: "chai",
+    email: 'chai@google.com'
+}
+
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeacherSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: "JS assignment",
+    fullTime: true,
+    __proto__: TeacherSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+
+Object.setPrototypeOf(TeacherSupport, Teacher)
+
+let anotherUserName = "ChaiAurCode       "
+
+String.prototype.trueLength = function() {
+    console.log(`${this}`)
+    console.log(`True length is: ${this.trim().length}`)
+}
+
+anotherUserName.trueLength()
+'vijay'.trueLength()
+'icetea'.trueLength()
